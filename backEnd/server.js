7af19f9711578c6ff6f3f62d02bb863c49c7beb2
Jesus-V <jesusviolante17@gmail.com>
2025-08-22 10:01:57 -0500

    after refresh

---
+++ backEnd/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../frontEnd")));
+
+// routes
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontEnd/views", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`listing on port ${port}`);
+});
